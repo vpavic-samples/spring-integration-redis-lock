e12b74f164168a1a84aecd96d739feff3b60434c
Vedran Pavic <vedran.pavic@gmail.com>
2017-03-24 22:22:22 +0100

    Initial commit

---
+++ src/main/java/sample/RedisLockApplication.java
@@ -0,0 +1,60 @@
+package sample;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.channel.MessageChannels;
+import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.endpoint.MethodInvokingMessageSource;
+import org.springframework.integration.redis.util.RedisLockRegistry;
+import org.springframework.integration.support.leader.LockRegistryLeaderInitiator;
+import org.springframework.integration.support.locks.LockRegistry;
+
+@SpringBootApplication
+public class RedisLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLockApplication.class, args);
+	}
+
+	@Bean
+	public RedisLockRegistry lockRegistry(RedisConnectionFactory connectionFactory) {
+		return new RedisLockRegistry(connectionFactory, "sample");
+	}
+
+	@Bean
+	public LockRegistryLeaderInitiator leaderInitiator(LockRegistry lockRegistry) {
+		return new LockRegistryLeaderInitiator(lockRegistry);
+	}
+
+	@Bean
+	public MessageSource<?> integerMessageSource() {
+		MethodInvokingMessageSource source = new MethodInvokingMessageSource();
+		source.setObject(new AtomicInteger());
+		source.setMethodName("getAndIncrement");
+		return source;
+	}
+
+	@Bean
+	public DirectChannel inputChannel() {
+		return new DirectChannel();
+	}
+
+	@Bean
+	public IntegrationFlow myFlow() {
+		return IntegrationFlows.from(integerMessageSource(), c -> c.poller(Pollers.fixedRate(100)))
+				.channel(this.inputChannel())
+				.filter((Integer p) -> p > 0)
+				.transform(Object::toString)
+				.channel(MessageChannels.queue())
+				.get();
+	}
+
+}
